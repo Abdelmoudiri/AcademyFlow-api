65095187795e22663f3592764cca2317213442ed
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-03-17 14:09:58 +0000

    ajouter un factory et seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'fname' => fake()->name(),
+            'lname' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement(['teacher', 'student']),
             'remember_token' => Str::random(10),
         ];
     }
